bbcc33a80e4fdc055dbdbef8ba6636b36be6e710
BonBonPhenomenon <bonnievujcich@gmail.com>
2026-01-29 09:21:58 +1300

    Add feature message 2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// BIT504 A2 Task 9
 		System.out.println("Initial commit on main");
-
+		System.out.println("Feature message 2");
 	}
 
 }
